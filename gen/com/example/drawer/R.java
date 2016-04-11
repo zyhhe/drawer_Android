b3e35da07b53aa05c3ec1bc3a3469458df28f2b2
zhjt58 <1105295391@qq.com>
2016-04-11 18:54:49 +0800

    最后一次新交

--- gen/com/example/drawer/R.java
+++ gen/com/example/drawer/R.java
@@ -198,10 +198,11 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int main_content=0x7f030001;
-        public static final int main_navigation_item=0x7f030002;
-        public static final int navigation_list=0x7f030003;
-        public static final int secondary_navigation_item=0x7f030004;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int main_content=0x7f030002;
+        public static final int main_navigation_item=0x7f030003;
+        public static final int navigation_list=0x7f030004;
+        public static final int secondary_navigation_item=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
